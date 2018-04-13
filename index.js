cd321e4a6716898754496fa09d2458f4d197045d
antitim <antitim@yandex.ru>
2018-04-14 01:13:04 +0300

    added settings validation

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const EventEmitter = require('events');
 
 class KtotamBot extends EventEmitter {
+  constructor (settings) {
+    super(settings);
+
+    if (!settings.message) throw new Error('Not specified "message" in settings');
+
+    this.settings = settings;
+  }
   message (message) {
     this.emit('message', {
       client: message.client,
-      text: 'Кто там?'
+      text: this.settings.message,
     });
   }
 }
